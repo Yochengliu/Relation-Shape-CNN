dd04e0e8f9b4de57b36ba260ee258918ecb06096
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-30 19:34:52 +0800

    Delete train_partseg.sh

--- train_partseg.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p log
-now=$(date +"%Y%m%d_%H%M%S")
-log_name="PartSeg_LOG_"$now""
-export CUDA_VISIBLE_DEVICES=0
-python -u train_partseg.py \
---config cfgs/config_msn_partseg.yaml \
-2>&1|tee log/$log_name.log &
