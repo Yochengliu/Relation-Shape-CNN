38ac7d1e1dc5e29ef44edac22bdfa28319c7ba30
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-30 19:34:02 +0800

    Delete train_cls.sh

--- train_cls.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-mkdir -p log
-now=$(date +"%Y%m%d_%H%M%S")
-log_name="Cls_LOG_"$now""
-export CUDA_VISIBLE_DEVICES=0
-python -u train_cls.py \
---config cfgs/config_ssn_cls.yaml \
-2>&1|tee log/$log_name.log &
